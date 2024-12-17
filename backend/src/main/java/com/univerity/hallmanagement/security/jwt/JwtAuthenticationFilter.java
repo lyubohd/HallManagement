cee3f2f2e8330f1e13906b426cc1879eaa9d07c2
lyubo <lyubomir.tomanov@gmail.com>
2024-12-17 19:04:08 +0200

    [HM-006] - Implement JWT Authorisation

--- backend/src/main/java/com/univerity/hallmanagement/security/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/univerity/hallmanagement/security/jwt/JwtAuthenticationFilter.java
@@ -4,8 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.experimental.NonFinal;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+                                    FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;
